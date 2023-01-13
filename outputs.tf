9172083a5979acb62036225b63c25884fcbbd3e0
tjmaru <30507792+tjmaru@users.noreply.github.com>
2023-01-13 16:10:25 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,6 @@
 output "subnet_id" {
   description = "Subnet ID"
   value = coalesce(
-    join("", azurerm_subnet.subnet.*.id),
-    join("", module.aws_vpc.public_subnets),
     join("", google_compute_subnetwork.subnet.*.id),
     "dummy"
   )
@@ -11,8 +9,6 @@ output "subnet_id" {
 output "vpc_id" {
   description = "VPC ID"
   value = coalesce(
-    join("", azurerm_virtual_network.vnet.*.name),
-    module.aws_vpc.vpc_id,
     join("", google_compute_network.network.*.self_link),
     "dummy"
   )
